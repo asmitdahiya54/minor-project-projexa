b588e4c6e043412f795df1ca1ad2122fc042308a
Asmit <destroyer13aa@gmail.com>
2026-02-23 11:47:23 +0530

    Added add student and view student functionality with Flask

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+students = []   # Temporary storage
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/add', methods=['GET', 'POST'])
+def add_student():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        course = request.form['course']
+
+        students.append({
+            'name': name,
+            'email': email,
+            'course': course
+        })
+
+        return redirect('/view')
+
+    return render_template('add_student.html')
+
+@app.route('/view')
+def view_students():
+    return render_template('view_students.html', students=students)
+
+if __name__ == '__main__':
     app.run(debug=True)
