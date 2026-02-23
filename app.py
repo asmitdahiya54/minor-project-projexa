6e47993e19f561a8ca174e1f279d7cc2283e2a3d
ManishChauhan12-prog <manishk79432@gmail.com>
2026-02-23 11:36:49 +0530

    Initial commit

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    https://github.com/asmitdahiya54/minor-project-projexa#:~:text=Settings-,minor%2Dproject%2Dprojexa,-Public
+    
 
+https://github.com/asmitdahiya54/minor-project-projexa#:~:text=Settings-,minor%2Dproject%2Dprojexa,-Public
