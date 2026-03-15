4517cc8125c06ce81bba769d1baca49f05a12ed0
ManishChauhan12-prog <manishk79432@gmail.com>
2026-03-15 13:51:19 +0530

    Connected dashboard template with flask

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 import os
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = "studentinfolite_secret_2024"
 
 @app.route("/")
-def home():
-    return "StudentInfoLite"
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
